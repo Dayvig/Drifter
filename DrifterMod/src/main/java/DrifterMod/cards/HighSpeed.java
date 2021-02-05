1caebfaa51f0264b174ee0749dec4bf56f385e27
Dayvig <49045434+Dayvig@users.noreply.github.com>
2021-02-05 00:09:55 -0500

    Added images
    added images for a handful of cards
    added cards
    other things

---
+++ DrifterMod/src/main/java/DrifterMod/cards/HighSpeed.java
@@ -0,0 +1,82 @@
+package DrifterMod.cards;
+
+import DrifterMod.DrifterMod;
+import DrifterMod.characters.TheDrifter;
+import DrifterMod.powers.DriftPower;
+import DrifterMod.powers.OverdrawNextTurn;
+import DrifterMod.powers.Speedup;
+import DrifterMod.powers.TempMaxHandSizeInc;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+
+import java.util.ArrayList;
+
+import static DrifterMod.DrifterMod.makeCardPath;
+
+public class HighSpeed extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Defend Gain 5 (8) block.
+     */
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = DrifterMod.makeID(HighSpeed.class.getSimpleName());
+    public static final String IMG = makeCardPath("Skill.png");
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDrifter.Enums.COLOR_YELLOW;
+
+    private static final int COST = 2;
+    private static final int MAGIC = 2;
+
+
+    // /STAT DECLARATION/
+
+
+    public HighSpeed() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (p.hasPower(Speedup.POWER_ID)){
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new DriftPower(p, p, magicNumber * p.getPower(Speedup.POWER_ID).amount)));
+        }
+        if (upgraded){
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new Speedup(p, p, 1), 1));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        upgradeName();
+        this.rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
